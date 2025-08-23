dec81c41606c9340d9d9eb8717dee28e40894038
FeyipitanIgbowu <feyiigbowu@gmail.com>
2025-08-23 19:59:35 +0100

    Fourth commit

---
+++ JavaScript/MenstrualCycleApp.js
@@ -0,0 +1,34 @@
+const prompt = require('prompt-sync')();
+
+function dateCollection(monthName, dayNumber){
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  monthName = monthName[0].toUpperCase() + monthName.slice(1).toLowerCase();
+ let year = 2025;
+   
+let date = new Date(`${monthName} ${dayNumber} ${year}`);
+return date.toLocaleDateString();
+}
+
+function checkNextPeriod(startDate, cycleLength){
+	nextPeriod = startDate + cycleLength
+return nextPeriod.toLocaleDateString();
+
+}
+
+function ovulationDate(startDate, cycleLength){
+  let ovulationDay = startDate + (cycleLength - 14);
+return ovulationDay.toLocaleDateString();
+}
+
+
+
+let response = (dateCollection("August", 1));
+console.log(response)
+
+let nextPeriod1 = prompt("When was your Period Start Date: ")
+let nextPeriod2 = prompt("What is your Cycle Length: ")
+console.log(dateCollection(nextPeriod1, nextPeriod2))
+
+let ovulationDate1 = prompt("When was your Period Start Date: ")
+let ovulationDate2 = prompt("What is your Cycle Length: ")
+console.log(OvulationDate(ovulationDate1, ovulationDate2))
