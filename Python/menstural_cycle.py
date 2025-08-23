077ff46cb4ec8a2da5431b9a0a2e309e278745bb
FeyipitanIgbowu <feyiigbowu@gmail.com>
2025-08-23 16:00:59 +0100

    Second commit

--- Python/menstural_cycle.py
+++ Python/menstural_cycle.py
@@ -2,21 +2,55 @@ from datetime import datetime, timedelta
 import calendar
 
 def date_collection(month_name, day_number):
-	if type(month_name) != str and type(day_number) != int:
+	if type(month_name) != str or type(day_number) != int:
 		raise ValueError("Invalid Data Type")
 		
 	if day_number <= 0:
 		raise ValueError("Zero's and negative numbers are not allowed")
 		
-	months = list(calendar.month_name)[1:]
-	if month_name.capitalize() != months:
+	months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	month_name = month_name.strip().capitalize()
+	if month_name not in months:
 		raise ValueError("Invalid Month")
 		
 	year = 2025
-	month_index = months.index(month_name.capitalize()) + 1
+	month_index = months.index(month_name) + 1
 	highest_days = calendar.monthrange(year, month_index)[1]
 	if day_number < 1 or day_number > highest_days:
 		raise ValueError("Invalid Day")
 
-	date_conversion = month_name + " " + str(day_number) + "2025"  
-	return datetime.strptime(date_conversion)
+	date_conversion = month_name + " " + str(day_number) + " 2025"  
+	return datetime.strptime(date_conversion,"%B %d %Y")
+
+def check_next_period(start_date, cycle_length):
+	if type(start_date) != int or type(cycle_length) != int:
+		raise ValueError("Invalid Data Type")
+		
+	if start_date <= 0 or cycle_length <= 0:
+		raise ValueError("Zero's and negative numbers are not allowed")
+	
+	start_date = datetime.today().replace(day=start_date)
+	next_period = start_date + timedelta(days=cycle_length)
+	return next_period.day
+	
+def ovulation_date(start_date, cycle_length):
+	if type(start_date) != int or type(cycle_length) != int:
+		raise ValueError("Invalid Data Type")
+	
+	if start_date <= 0 or cycle_length <= 0:
+		raise ValueError("Zero's and negative numbers are not allowed")
+	
+	start_date = datetime.today().replace(day=start_date)
+	ovulation_date = start_date + timedelta(days=(cycle_length - 14))
+	return ovulation_date.day
+	
+def fertile_window(start_date, cycle_length):
+	start_date = datetime.today().replace(day=start_date)
+	ovulation_date = start_date + timedelta(days=(cycle_length - 14))
+	
+	fertile_window = ovulation_date - timedelta(days=2), ovulation_date + timedelta(days=2)
+    
+    
+    
+    
+    
--- Python/test_menstural_cycle.py
+++ Python/test_menstural_cycle.py
@@ -1,10 +1,14 @@
 import unittest
+from datetime import datetime, timedelta
 import menstural_cycle
 
-class TestMensturalCycle(unittest.TestCase):
-	def test_for_invalid_data_type_of_date_and_month(self):
+class TestDateCollection(unittest.TestCase):
+	def test_for_invalid_data_type_of_date(self):
 		self.assertRaises(ValueError,menstural_cycle.date_collection,23, "Feyi")
 		
+	def test_for_invalid_data_type_of_month(self):
+		self.assertRaises(ValueError,menstural_cycle.date_collection,"Feyi", 23)
+
 	def test_for_zero_input(self):
 		self.assertRaises(ValueError,menstural_cycle.date_collection, "Febuarry", 0)
 
@@ -17,4 +21,43 @@ class TestMensturalCycle(unittest.TestCase):
 	def test_for_invalid_date_in_february(self):
 		self.assertRaises(ValueError,menstural_cycle.date_collection, "Febuarry", 30)
 
+	def test_that_the_function_works(self):
+		response = menstural_cycle.date_collection("September", 1)
+		expected = datetime(2025, 9, 1)
+		self.assertEqual(response, expected)
+		
+class TestForNextPeriod(unittest.TestCase):
+	def test_for_invalid_data_type_for_cycle_length(self):
+		self.assertRaises(ValueError,menstural_cycle.check_next_period,20 , "Feyi")
+	
+	def test_for_invalid_data_type_for_start_date(self):
+		self.assertRaises(ValueError,menstural_cycle.check_next_period, "Feyi", 10)
 
+	def test_for_zero_error(self):
+		self.assertRaises(ValueError,menstural_cycle.check_next_period,0 , 0)
+		
+	def test_for_negative_input(self):
+		self.assertRaises(ValueError,menstural_cycle.check_next_period, -1 , -5)
+	
+	def test_that_function_works(self):
+		response = menstural_cycle.check_next_period(1, 27)
+		self.assertEqual(response, 28)
+		
+class TestForOvulationDate(unittest.TestCase):
+	def test_for_invalid_data_type_for_cycle_length(self):
+		self.assertRaises(ValueError,menstural_cycle.ovulation_date,20 , "Feyi")
+	
+	def test_for_invalid_data_type_for_start_date(self):
+		self.assertRaises(ValueError,menstural_cycle.ovulation_date, "Feyi", 1)
+
+	def test_for_zero_error(self):
+		self.assertRaises(ValueError,menstural_cycle.ovulation_date,0 , 0)
+		
+	def test_for_negative_input(self):
+		self.assertRaises(ValueError,menstural_cycle.ovulation_date, -1 , -5)
+	
+	def test_that_function_works(self):
+		response = menstural_cycle.ovulation_date(1, 27)
+		self.assertEqual(response, 14)
+
+	
